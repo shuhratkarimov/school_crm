6ac061b47decdf5654b98ad77097a1c8d84951a8
shuhratkarimov <shuhratkarimov.dev@gmail.com>
2026-03-09 20:34:01 +0500

    front updates added

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -2,23 +2,20 @@ import { DotLottieReact } from "@lottiefiles/dotlottie-react";
 
 const LottieLoading = () => {
   return (
-    // <div className="fixed inset-0 z-50 flex items-center justify-center backdrop-blur-sm bg-black/10">
-    //   <div className="flex flex-col items-center justify-center">
-    //     <DotLottieReact
-    //       src="/animations/loading.lottie"
-    //       autoplay={true}
-    //       loop
-    //       style={{
-    //         width: 400,
-    //         height: 400,
-    //         display: "block",
-    //         margin: "150px auto",
-    //       }}
-    //     />
-    //   </div>
-    // </div>
-    <div>
-      <h1>Loading...</h1>
+    <div className="fixed inset-0 z-50 flex items-center justify-center backdrop-blur-sm bg-black/10">
+      <div className="flex flex-col items-center justify-center">
+        <DotLottieReact
+          src="/animations/loading.lottie"
+          autoplay={true}
+          loop
+          style={{
+            width: 400,
+            height: 400,
+            display: "block",
+            margin: "150px auto",
+          }}
+        />
+      </div>
     </div>
   );
 };
